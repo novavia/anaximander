1922a21bae870687b474541ee84349216ec56542
JD Margulici <jdmargulici@gmail.com>
2017-10-27 12:41:49 -0700

    Corrected bug triggered by the addition of column families to existing bigtable tables.

--- anaximander/data/gcbigtable.py
+++ anaximander/data/gcbigtable.py
@@ -488,7 +488,7 @@ class BigTableQuery(DataQuery):
     def read_row(self, row):
         attrs = ChainMap(*[{k.decode('utf-8'): v[0].value.decode('utf-8')
                             for k, v in row.cells[family].items()}
-                           for family in self.columns])
+                           for family in self.columns if family in row.cells])
         return tuple(attrs[k] if k in attrs else
                      self.table.schema.fields[k].default for k in self.fields)
 
