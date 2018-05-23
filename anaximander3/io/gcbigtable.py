e7d1a8cbdfc5fa4bf96673307b52ef3eebab6935
JD Margulici <jdmargulici@gmail.com>
2018-05-23 11:09:37 -0700

    IO package.

---
+++ anaximander3/io/gcbigtable.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module provides an interface from the data package to Google BigQuery.
+
+This module is part of the Anaximander project.
+Copyright (C) Novavia Solutions, LLC.
+"""
+
+# =============================================================================
+# Imports and constants
+# =============================================================================
+
+from collections import OrderedDict
+from concurrent.futures import ThreadPoolExecutor
+import datetime as dt
+
+import attr
+from google.cloud._helpers import _to_bytes
+from google.cloud.bigtable._generated import (
+    bigtable_pb2 as data_messages_v2_pb2)
+from grpc._channel import _Rendezvous
+from google.cloud.bigtable.client import Client
+import google.cloud.bigtable.table as gc_big_table
+from google.cloud.bigtable.column_family import MaxAgeGCRule
+from google.cloud.bigtable.row_data import PartialRowsData
+from google.cloud.bigtable.instance import Instance
+from google.cloud.happybase.pool import ConnectionPool
+from google.cloud.bigtable.row_filters import ColumnQualifierRegexFilter, \
+    RowFilterUnion
+
+from ..utilities import xprops, nxrange as rge, functions as fun
+from ..data.nxschema import Schema, MultiSchema, TimeSeriesIndex
+from .table import DataTable, DataQuery, DataQueryException, \
+    DataTableWriteException, DataTableAdminException, EmptyQueryException
+
+__all__ = ['BigTableDataTable', 'BigTableQuery', 'BigTableQueryException',
+           'BigTableInsertException', 'Client', 'Instance']
+
+# =============================================================================
+# Monkeypatching of Table
+# =============================================================================
+
+
+def _create_row_request(table_name, row_key=None, start_key=None, end_key=None,
+                        filter_=None, limit=None, reverse=False):
+    """Creates a request to read rows in a table.
+
+    :type table_name: str
+    :param table_name: The name of the table to read from.
+
+    :type row_key: bytes
+    :param row_key: (Optional) The key of a specific row to read from.
+
+    :type start_key: bytes
+    :param start_key: (Optional) The beginning of a range of row keys to
+                      read from. The range will include ``start_key``. If
+                      left empty, will be interpreted as the empty string.
+
+    :type end_key: bytes
+    :param end_key: (Optional) The end of a range of row keys to read from.
+                    The range will not include ``end_key``. If left empty,
+                    will be interpreted as an infinite string.
+
+    :type filter_: :class:`.RowFilter`
+    :param filter_: (Optional) The filter to apply to the contents of the
+                    specified row(s). If unset, reads the entire table.
+
+    :type limit: int
+    :param limit: (Optional) The read will terminate after committing to N
+                  rows' worth of results. The default (zero) is to return
+                  all results.
+
+    :type reverse: bool
+    :param reverse: if True, then the request is closed wrt. end key and
+        open wrt. to the start key.
+
+    :rtype: :class:`data_messages_v2_pb2.ReadRowsRequest`
+    :returns: The ``ReadRowsRequest`` protobuf corresponding to the inputs.
+    :raises: :class:`ValueError <exceptions.ValueError>` if both
+             ``row_key`` and one of ``start_key`` and ``end_key`` are set
+    """
+    request_kwargs = {'table_name': table_name}
+    if (row_key is not None and
+            (start_key is not None or end_key is not None)):
+        raise ValueError('Row key and row range cannot be '
+                         'set simultaneously')
+    range_kwargs = {}
+    if start_key is not None or end_key is not None:
+        if start_key is not None:
+            if reverse is True:
+                range_kwargs['end_key_closed'] = _to_bytes(start_key)
+            else:
+                range_kwargs['start_key_closed'] = _to_bytes(start_key)
+        if end_key is not None:
+            if reverse is True:
+                range_kwargs['start_key_open'] = _to_bytes(end_key)
+            else:
+                range_kwargs['end_key_open'] = _to_bytes(end_key)
+    if filter_ is not None:
+        request_kwargs['filter'] = filter_.to_pb()
+    if limit is not None:
+        request_kwargs['rows_limit'] = limit
+
+    message = data_messages_v2_pb2.ReadRowsRequest(**request_kwargs)
+
+    if row_key is not None:
+        message.rows.row_keys.append(_to_bytes(row_key))
+
+    if range_kwargs:
+        message.rows.row_ranges.add(**range_kwargs)
+
+    return message
+
+
+gc_big_table._create_row_request = _create_row_request
+
+
+def read_rows(self, start_key=None, end_key=None, limit=None,
+              filter_=None, reverse=False):
+    """Read rows from this table.
+
+    :type start_key: bytes
+    :param start_key: (Optional) The beginning of a range of row keys to
+                      read from. The range will include ``start_key``. If
+                      left empty, will be interpreted as the empty string.
+
+    :type end_key: bytes
+    :param end_key: (Optional) The end of a range of row keys to read from.
+                    The range will not include ``end_key``. If left empty,
+                    will be interpreted as an infinite string.
+
+    :type limit: int
+    :param limit: (Optional) The read will terminate after committing to N
+                  rows' worth of results. The default (zero) is to return
+                  all results.
+
+    :type filter_: :class:`.RowFilter`
+    :param filter_: (Optional) The filter to apply to the contents of the
+                    specified row(s). If unset, reads every column in
+                    each row.
+
+    :type reverse: bool
+    :param reverse: if True, then the request is closed wrt. end key and
+        open wrt. to the start key.
+
+    :rtype: :class:`.PartialRowsData`
+    :returns: A :class:`.PartialRowsData` convenience wrapper for consuming
+              the streamed results.
+    """
+    request_pb = _create_row_request(
+        self.name, start_key=start_key, end_key=end_key, filter_=filter_,
+        limit=limit, reverse=reverse)
+    client = self._instance._client
+    response_iterator = client._data_stub.ReadRows(request_pb)
+    # We expect an iterator of `data_messages_v2_pb2.ReadRowsResponse`
+    return PartialRowsData(response_iterator)
+
+
+gc_big_table.Table.read_rows = read_rows
+
+# =============================================================================
+# Table implementation
+# =============================================================================
+
+
+class BigTableAdminException(DataTableAdminException):
+    pass
+
+
+class BigTableInsertException(DataTableWriteException):
+    pass
+
+
+@attr.s
+class BigTableDataTable(DataTable):
+    instance = attr.ib(validator=attr.validators.instance_of(Instance))
+    name = attr.ib(validator=attr.validators.instance_of(str))
+    schema = attr.ib(validator=attr.validators.instance_of((Schema,
+                                                            MultiSchema)),
+                     convert=lambda s: s() if isinstance(s, type) else s)
+    # Data retention time, in days, defaulting to None (indefinite retention)
+    retention = attr.ib(None, repr=False)
+    threadpoolsize = 250  # size of thread pool for inserts
+
+    @xprops.cachedproperty
+    def table(self):
+        """Caches an instance of table in the bigtable API."""
+        table = self.instance.table(self.name)
+        return table
+
+    @property
+    def table_id(self):
+        return self.table.table_id
+
+    @property
+    def client(self):
+        return self.instance._client
+
+    @property
+    def project(self):
+        return self.client.project
+
+    @property
+    def gc_rule(self):
+        """Garbage collection rule, implementing retention policy."""
+        if self.retention is not None:
+            return MaxAgeGCRule(dt.timedelta(self.retention))
+
+    @xprops.cachedproperty
+    def pool(self):
+        """A connection pool from the happybase API."""
+        return ConnectionPool(1, instance=self.instance)
+
+    @xprops.cachedproperty
+    def reverse(self):
+        """If True, then keys are accumulated in reverse order.
+
+        This influences how queries are processed.
+        """
+        try:
+            assert isinstance(self.schema.index, TimeSeriesIndex)
+        except (AttributeError, AssertionError):
+            return False
+        else:
+            return True
+
+    def __exists__(self):
+        """Tests existence of the resource. Must return True or False."""
+        return self.name in [t.table_id for t in self.instance.list_tables()]
+
+    def __empty__(self):
+        """Tests whether the resource is empty or not."""
+        try:
+            next(self.table.sample_row_keys())
+        except StopIteration:
+            return True
+        except:
+            return False
+        else:
+            return True
+
+    def __create__(self, **kwargs):
+        rval = self.table.create()
+        gc_rule = self.gc_rule
+        if isinstance(self.schema, Schema):
+            data = self.table.column_family('data', gc_rule=gc_rule)
+            data.create()
+        elif isinstance(self.schema, MultiSchema):
+            for f in self.schema.families:
+                family = self.table.column_family(f, gc_rule=gc_rule)
+                family.create()
+        return rval
+
+    def __drop__(self, force=False, **kwargs):
+        return self.table.delete()
+
+    def __reset_retention__(self):
+        if not self.exists:
+            msg = "Cannot run function on non-existing table {0}."
+            raise BigTableAdminException(msg.format(self))
+        gc_rule = self.gc_rule
+        for f in self.table.list_column_families().values():
+            f.gc_rule = gc_rule
+            f.update()
+
+    def __query__(self, *columns, **kwargs):
+        return BigTableQuery(self, *columns, **kwargs)
+
+    def __insert__(self, record, retry=True, **kwargs):
+        rowkey = record.key
+        row = self.table.row(rowkey)
+        dump = record.to_dict()
+        for family in self.table.list_column_families():
+            data = dump.get(family, {})
+            for k, v in data.items():
+                row.set_cell(family, k.encode('utf-8'), str(v).encode('utf-8'))
+        try:
+            row.commit()
+        # Single retry
+        except _Rendezvous:
+            if retry:
+                self.__insert__(record, retry=False, **kwargs)
+            else:
+                raise
+
+    def __append__(self, logs, **kwargs):
+        records = list(logs)
+        n = self.threadpoolsize
+        with ThreadPoolExecutor(n) as executor:
+            executor.map(self.__insert__, records)
+
+    # Redefinition of DataTable.insert to take advantage of threads
+    def insert(self, *records, **kwargs):
+        """Inserts one or more records into table."""
+        n = self.threadpoolsize
+        with ThreadPoolExecutor(n) as executor:
+            executor.map(self.__insert__, records)
+
+    def __record__(self, idx, **kwargs):
+        rowkey = self.schema.rowkey(idx)
+        row = self.table.read_row(rowkey.encode())
+        if row is None:
+            raise EmptyQueryException()
+        data = {f: {k.decode(): v[0].value.decode() for k, v in d.items()}
+                for f, d in row.cells.items()}
+        return data
+
+
+class BigTableQueryException(DataQueryException):
+    pass
+
+
+class BigTableMaxRowsException(BigTableQueryException):
+    """Raised when queries reach specified maxrows."""
+    pass
+
+
+class BigTableQuery(DataQuery):
+
+    def __init__(self, *columns, **quargs):
+        super().__init__(*columns, **quargs)
+        if 'id' not in quargs:
+            msg = "A BigTable Query must specify an id range."
+            raise BigTableQueryException(msg)
+
+    @xprops.cachedproperty
+    def rowfilter(self):
+        """Returns a row filter to be applied to query reads."""
+        colfilters = [ColumnQualifierRegexFilter(c.encode('utf-8'))
+                      for c in self.schema]
+        return RowFilterUnion(colfilters)
+
+    def rowkeypairs(self):
+        """A generator of rowkey pairs to slice the table."""
+        if isinstance(self.id_range, rge.Level):
+            id_range = [self.id_range.level]
+        else:
+            id_range = self.id_range
+        lower, upper = self.dt_range
+        for id_ in id_range:
+            start_key = self.schema.rowkey((id_, lower))
+            end_key = self.schema.rowkey((id_, upper))
+            yield (start_key, end_key)
+
+    def read_row(self, row):
+        data = {f: {k.decode(): v[0].value.decode() for k, v in d.items()}
+                for f, d in row.cells.items()}
+        idx = self.schema.rowidx(row.row_key.decode())
+        return (idx, data)
+
+    def first(self, **kwargs):
+        kwargs['limit'] = 1
+        return super().first(**kwargs)
+
+    def __fetch__(self, limit=None):
+        """Fetch primitive.
+
+        Params:
+            limit (int): limits the number of rows per id scanned by Bigtable.
+        """
+        rowkeypairs = list(self.rowkeypairs())
+        limit = int(fun.get(limit, 1e5))
+
+        def row_group(keys):
+            start_key, end_key = keys
+            return self.table.table.read_rows(start_key,
+                                              end_key,
+                                              filter_=self.rowfilter,
+                                              reverse=self.table.reverse,
+                                              limit=limit)
+
+        with ThreadPoolExecutor(len(rowkeypairs)) as executor:
+            row_groups = executor.map(row_group, rowkeypairs)
+
+        for g in row_groups:
+            while True:
+                g._rows = OrderedDict()
+                try:
+                    g.consume_next()
+                except StopIteration:
+                    break
+                for row in g.rows.values():
+                    yield self.read_row(row)
---
+++ tests/units/io/test_gcbigtable.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test module for BigQuery interface.
+
+This module is part of the Anaximander project.
+Copyright (C) Novavia Solutions, LLC.
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+import os.path
+import time
+
+import pandas as pd
+import pytest
+
+from google.cloud import bigtable as bt
+from grpc._channel import _Rendezvous
+
+import anaximander3 as nx
+from anaximander3.utilities import nxtime, nxrange as rge
+from anaximander3.data import nxcolumns as cln, nxschema as sch, \
+    datalogs as dtl, records as rec
+from anaximander3.io import gcbigtable as gbt
+
+#PROJECT_ID = 'anaximander-tests'
+#INSTANCE_ID = 'testinstance'
+PROJECT_ID = 'infinite-uptime-1232'
+INSTANCE_ID = 'testinstance'
+INSTANCE_LOC = 'us-central1-c'
+
+NXPATH = os.path.dirname(nx.__path__[0])
+TEST_DATA_DIR = os.path.join(NXPATH, 'tests/data')
+LOGFILE_PATH = os.path.join(TEST_DATA_DIR, 'featurelog.csv')
+
+FEATURE_IDS = ['88:4A:EA:69:DF:A2', '68:9E:19:07:DE:C3']
+FEATURE_TME = ['2016-9-14 10:00', '2016-9-14 10:05']
+MAC_PATTERN = '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+DEVICES = ['88:4A:EA:69:DF:A2', '68:9E:19:07:DE:C3']
+
+# =============================================================================
+# Environment
+# =============================================================================
+
+
+class BtSchema(sch.SampleLogsSchema):
+    accel_x = cln.Measurement()
+    accel_y = cln.Measurement()
+
+    def __rowkey__(self, index):
+        id, dt = index
+        postfix = str(int(1e6 * (nxtime.MAX_TIMESTAMP - dt.timestamp())))
+        return '#'.join((id[::-1], postfix))
+
+    def __rowidx__(self, key):
+        id, postfix = key.split('#')
+        dt = pd.Timestamp(1e6 * nxtime.MAX_TIMESTAMP - int(postfix),
+                          tz='UTC', unit='us')
+        return (id[::-1], dt)
+
+
+@pytest.fixture(scope="module")
+def featurelog():
+    """Returns a nominal dataframe."""
+    dataframe = pd.read_csv(LOGFILE_PATH)
+    dataframe.rename(columns={'timestamp': 'datetime',
+                              'device': 'id',
+                              'Feature_Value_1': 'accel_x',
+                              'Feature_Value_2': 'accel_y'},
+                     inplace=True)
+    log = dtl.DataLog(dataframe, schema=BtSchema,
+                      id_range=FEATURE_IDS, dt_range=FEATURE_TME)
+    return log
+
+
+def cleanup(instance):
+    """Cleans up the supplied BigQuery dataset."""
+    for table in instance.list_tables():
+        table.delete()
+    instance.delete()
+
+
+@pytest.fixture(scope="session")
+def instance():
+    """Creates a bigtable instance for testing purposes."""
+    btclient = bt.Client(project=PROJECT_ID, admin=True)
+    instance = btclient.instance(INSTANCE_ID, INSTANCE_LOC)
+    instance.display_name = INSTANCE_ID
+    try:
+        instance.create()
+    except _Rendezvous:
+        pass
+    time.sleep(10)
+    yield instance
+    cleanup(instance)
+
+
+@pytest.fixture(scope="module")
+def ghost_table(instance):
+    """Yields uncreated table instance."""
+    return gbt.BigTableDataTable(instance, 'ghost', BtSchema)
+
+
+@pytest.fixture(scope="module")
+def empty_table(instance):
+    """Yields an empty table to test insertions and appends."""
+    table = gbt.BigTableDataTable(instance, 'empty', BtSchema)
+    table.create(warn=False, overwrite=True, force=True)
+    return table
+
+
+@pytest.fixture(scope="module")
+def full_table(instance, featurelog):
+    """Yields a populated table to test queries."""
+    table = gbt.BigTableDataTable(instance, 'full', BtSchema)
+    table.create(warn=False, overwrite=True, force=True)
+    # Populates the table with some data
+    table.append(featurelog)
+    return table
+
+# =============================================================================
+# Test Cases
+# =============================================================================
+
+# Specifies that tests are skipped if tester is not online.
+pytestmark = [pytest.mark.online, pytest.mark.gcloud, pytest.mark.bigtable]
+
+
+def test_table_instantiation(ghost_table):
+    assert ghost_table.table_id == 'ghost'
+    assert ghost_table.reverse
+
+
+def test_keymaker(featurelog, ghost_table):
+    record = featurelog[0]
+    key = '3C:ED:70:91:E9:86#2628597572200000'
+    assert ghost_table.schema.rowkey(record.idx) == key
+
+
+def test_record(full_table):
+    idx = ('88:4A:EA:69:DF:A2',
+           pd.Timestamp('2016-09-14 10:02:27.800000+00:00'))
+    record = full_table.record(idx)
+    assert record.id == '88:4A:EA:69:DF:A2'
+    keys = ('88:4A:EA:69:DF:A2',
+            pd.Timestamp('2016-09-14 10:02:27.900000+00:00'))
+    with pytest.raises(KeyError):
+        full_table.record(*keys)
+
+
+def test_insert(empty_table, featurelog):
+    record = featurelog[0]
+    empty_table.insert(record)
+    row = empty_table.table.read_row(record.key.encode('utf-8'))
+    assert isinstance(row, bt.row_data.PartialRowData)
+
+
+def test_append(empty_table, featurelog):
+    empty_table.append(featurelog)
+    rowdata = empty_table.table.read_rows()
+    rowdata.consume_all()
+    assert len(rowdata.rows) >= len(featurelog)
+
+
+def test_query(full_table, featurelog):
+    query = full_table.query(id=DEVICES)
+    assert query.schema == BtSchema()
+    assert len(list(query.fetch())) == len(featurelog)
+    assert len(list(query.fetch(limit=1))) == 2
+
+
+def test_first(full_table):
+    query = full_table.query(id='88:4A:EA:69:DF:A2')
+    record = query.first()
+    assert isinstance(record, rec.Record)
+    assert record.id == '88:4A:EA:69:DF:A2'
+    assert record.datetime == pd.Timestamp('2016-09-14 10:04:58.700000+00:00')
+    query = full_table.query(id='88:4A:EA:69:DF:A2',
+                             datetime=(None, '2016-09-14 10:03:00'))
+    record = query.first()
+    assert record.datetime == pd.Timestamp('2016-09-14 10:02:27.800000+00:00')
+
+
+def test_log(full_table):
+    query = full_table.query(id='68:9E:19:07:DE:C3')
+    log = query.data()
+    assert isinstance(log, dtl.DataSequence)
+    assert log.id_range == '68:9E:19:07:DE:C3'
+    assert len(log) == 3
+    query = full_table.query(id=DEVICES)
+
+
+def test_fields(full_table):
+    query = full_table.query('accel_x', id='68:9E:19:07:DE:C3')
+    log = query.data()
+    assert log.schema == BtSchema('accel_x')
+
+
+if __name__ == '__main__':
+    pytest.main([__file__, '-x', '--pdb', '--bigtable'])
