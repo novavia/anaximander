8e75d33726f4c033f69eb39dee6bed09ff077714
JD Margulici <jdm@novavia.us>
2025-01-29 18:15:01 -0800

    Implemented KwargMap

---
+++ poc/anaximander/tests/utils/test_utils.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+
+import pytest
+from anaximander.utils import KwargMap
+
+
+def test_kwarg_map():
+    km_0 = KwargMap(a=1, b=2)
+    assert dict(km_0) == {"a": 1, "b": 2}
+    assert str(km_0) == "a=1, b=2"
+
+    # only string keys
+    with pytest.raises(TypeError):
+        km_0[1] = 2  # type: ignore
+
+    # pass a context (a mapping)
+    km_1 = KwargMap(km_0, a=".a")
+    assert km_1["a"] == 1
+    assert str(km_1) == "a=1"
+
+    # pass no context (defaults to local)
+    km_2 = KwargMap(a=".km_0")
+    assert km_2["a"] == km_0
+
+    # Now use an object as context
+    @dataclass
+    class C:
+        x: int
+        y: int
+
+    # test the "_" key
+    km_3 = KwargMap(C(1, 2), _=["x", "y"])
+    assert km_3["x"] == 1
+    assert km_3["y"] == 2
+
+    # test key deletion
+    del km_3["x"]
+    assert str(km_3) == "y=2"
+    with pytest.raises(KeyError):
+        del km_3["x"]
+
+    # test insertion, including redundant keys
+    km_3["y"] = ".y"
+    assert km_3.data == {"y": ".y"}
+    km_3["_"] = ["x"]
+    assert str(km_3) == "y=2, x=1"
+
+    # test sorting
+    km_3.sort()
+    assert str(km_3) == "x=1, y=2"
+    km_3.sort(["y", "x"])
+    assert str(km_3) == "y=2, x=1"
+    km_3.sort(["y", "x"], reverse=True)
+    assert str(km_3) == "x=1, y=2"
+    with pytest.raises(KeyError):
+        km_3.sort(["x", "y", "z"])
+
+    # test calling
+    km_4 = km_3("x")
+    assert str(km_4) == "x=1"
---
+++ poc/anaximander/utils/__init__.py
@@ -0,0 +1,158 @@
+import inspect
+from collections import UserDict
+from collections.abc import Mapping, Sequence
+from typing import Any, Callable
+
+
+class KwargMap(UserDict[str, Any]):
+    """Facility to hold keyword arguments and pass them to a function or stringify them.
+
+    The __init__ method accepts a context argument, typically an object from which attributes
+    will be looked up. If the context is None, then locals() will serve as the default context.
+    The interface accepts the special syntax ".name" in the values to indicate that
+    the kwarg map will look for an attribute named "name" in the context. A further shortcut
+    is to pass a list of argument names to the special key "_", such that:
+        KwargMap(obj, _=["a", "b"])
+    is equivalent to:
+        KwargMap(obj, a=".a", b=".b")
+    and exposes the mapping {"a": obj.a, "b": obj.b}
+    KwargMap also implements key sorting, preserving insertion order by default. The sort
+    method will modify sorting in place.
+    Finally, KwargMap implements __call__, which by default will return a new instance of
+    KwargMap with the same attributes. However the call can be used to specify a subset of
+    keys, in order, hence operating a subselection.
+    """
+
+    def __init__(self, context: Any = None, *args: Any, **kwargs: Any):
+        if context is None:
+            caller_frame = inspect.stack()[1]
+            context = caller_frame.frame.f_locals
+        self.context = context
+        self._key_sequence: list[str] = []  # type: ignore
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key: str, item: Any) -> None:
+        if not isinstance(key, str):
+            raise TypeError("Keys must be strings")
+        _keys: list[str] = list(self.data.get("_", []))
+        if key == "_":
+            try:
+                item = tuple(item)
+                assert all(isinstance(i, str) for i in item)
+                if not item:
+                    return
+            except (ValueError, TypeError, AssertionError):
+                raise TypeError("_ only maps to a sequence of strings.")
+            for k in _keys:
+                self._key_sequence.remove(k)
+        elif key in _keys:
+            _keys.remove(key)
+            self._key_sequence.remove(key)
+            if _keys:
+                self.data["_"] = tuple(_keys)
+            else:
+                del self.data["_"]
+        super().__setitem__(key, item)
+        if key not in self._key_sequence:
+            if key == "_":
+                self._key_sequence.extend(item)
+            else:
+                self._key_sequence.append(key)
+
+    def _getitem_from_context(self, actual_key: str):
+        """Primitive for looking up keys in the context."""
+        try:
+            return getattr(self.context, actual_key)
+        except AttributeError:
+            if isinstance(self.context, Mapping):
+                return self.context[actual_key]
+            else:
+                raise
+
+    def __getitem__(self, key: str) -> Any:
+        try:
+            value = self.data[key]
+            if isinstance(value, str) and value.startswith("."):
+                real_key = value[1:]
+                return self._getitem_from_context(real_key)
+            else:
+                return value
+        except KeyError:
+            if key in self.data.get("_", []):
+                return self._getitem_from_context(key)
+            else:
+                raise
+
+    def __delitem__(self, name: str) -> None:
+        if name == "_":
+            keys = self.data.pop("_")
+            for key in keys:
+                self._key_sequence.remove(key)
+            return
+        if name not in self._key_sequence:
+            raise KeyError(name)
+        _keys = list(self.data.get("_", []))
+        if name in _keys:
+            _keys.remove(name)
+            if _keys:
+                self.data["_"] = tuple(_keys)
+            else:
+                del self.data["_"]
+        else:
+            del self.data[name]
+        self._key_sequence.remove(name)
+
+    def __iter__(self):
+        return iter(self._key_sequence)
+
+    def __len__(self):
+        return len(self._key_sequence)
+
+    def sort(
+        self,
+        key: Sequence[str] | Callable[[str], Any] | None = None,
+        reverse: bool = False,
+        _remove_missing_keys: bool = False,
+    ) -> None:
+        """Sorts the KwargMap according to a specified key sequence.
+
+        Args:
+            key: A sequence of keys defining the desired order. Keys in the mapping not present
+                in the sequence will be appended at the end in their original order. Keys in the
+                sequence but not in the mapping will raise an error. Alternatively, takes the
+                same argument as list.sort(), i.e. a function of one argument used to extract a
+                comparison key from each list element.
+            reverse: Whether to sort in descending order.
+            _remove_missing_keys: used by the __call__ method to eliminate keys not supplied in
+                case key is a sequence of strings.
+        """
+        if isinstance(key, Sequence):
+            key_sequence = list(key)
+            if extra_keys := set(key_sequence) - set(self._key_sequence):
+                msg = f"Cannot sort with unknown keys: {extra_keys}"
+                raise KeyError(msg)
+            keys_not_in_sequence = [k for k in self._key_sequence if k not in key_sequence]
+            if _remove_missing_keys:
+                for k in keys_not_in_sequence:
+                    del self[k]
+                self._key_sequence = key_sequence
+            else:
+                new_key_sequence = key_sequence + keys_not_in_sequence
+                self._key_sequence = new_key_sequence
+            if reverse:
+                self._key_sequence.reverse()
+        else:
+            self._key_sequence.sort(key=key, reverse=reverse)
+
+    def __call__(self, *keys):
+        """Returns a new instance, restricted to the specified keys, and sorted accordingly."""
+        copy = type(self)(self.context, **self.data)
+        if keys:
+            copy.sort(keys, _remove_missing_keys=True)
+        return copy
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(context={self.context}, data={self.data})"
+
+    def __str__(self):
+        return ", ".join(f"{k}={v}" for k, v in self.items())
